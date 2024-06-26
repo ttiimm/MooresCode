ec2ad6a0a5deed881dc10382fecad2f459b2b649
Tim Likarish <likarish@gmail.com>
2024-06-25 22:59:00 -0400

    [Design] WIP got send button working how i want

--- app/src/main/java/net/ttiimm/morsecode/MainActivity.kt
+++ app/src/main/java/net/ttiimm/morsecode/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.DrawableRes
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -12,9 +13,11 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -25,11 +28,14 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import net.ttiimm.morsecode.ui.theme.MorseCodeTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -49,8 +55,11 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MessageLayout(modifier: Modifier = Modifier) {
+    var messageInput by remember { mutableStateOf("") }
+    var messageSending by remember { mutableStateOf("") }
+
     Column(
-        modifier = Modifier
+        modifier = modifier
             .statusBarsPadding()
             .padding(horizontal = 40.dp)
             .verticalScroll(rememberScrollState())
@@ -58,41 +67,49 @@ fun MessageLayout(modifier: Modifier = Modifier) {
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        Text(
-            text = stringResource(R.string.enter_message),
-            modifier = Modifier
-                .padding(bottom = 16.dp, top = 40.dp)
-                .align(alignment = Alignment.Start)
+        MessageInput(
+            message = messageInput,
+            onMessageChange = { messageInput = it },
+            doSend = {
+                messageSending = messageInput;
+                messageInput = ""
+            },
+            modifier = modifier
+                .padding(bottom = 32.dp, top = 40.dp)
+                .fillMaxWidth()
         )
 
-        MessageInput(modifier = Modifier
-            .padding(bottom = 32.dp)
-            .fillMaxWidth())
+        Text (
+            text = stringResource(R.string.sending, messageSending),
+            style = MaterialTheme.typography.displaySmall
+        )
     }
 }
 
 @Composable
-fun MessageInput(modifier: Modifier = Modifier) {
+fun MessageInput(
+    message: String,
+    onMessageChange: (String) -> Unit,
+    doSend: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    val focusManager = LocalFocusManager.current;
     Column(modifier = modifier) {
-        var messageInput by remember { mutableStateOf("") }
         TextField(
-            value = messageInput,
-            onValueChange = { messageInput = it },
+            label = { Text(stringResource(R.string.enter_message)) },
+            value = message,
+            onValueChange = onMessageChange,
             modifier = modifier,
             keyboardOptions = KeyboardOptions(
                 imeAction = ImeAction.Send
-            )
+            ),
+            keyboardActions = KeyboardActions(
+                onSend =  {
+                    focusManager.clearFocus();
+                    doSend();
+                }
+            ),
         )
-
-        Button(
-            modifier = modifier,
-            enabled = true,
-            onClick = {},
-        ) {
-            Text(
-                stringResource(R.string.send)
-            )
-        }
     }
 }
 
