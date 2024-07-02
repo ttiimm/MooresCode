884594b2175bdf63b44e970c93c97566cb99b7f2
Tim Likarish <likarish@gmail.com>
2024-07-02 17:20:39 -0400

    [UI] Bunch of theme/style settings

---
+++ app/src/main/java/net/ttiimm/morsecode/ui/theme/Shape.kt
@@ -0,0 +1,10 @@
+package net.ttiimm.morsecode.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Shapes
+import androidx.compose.ui.unit.dp
+
+val Shapes = Shapes(
+    small = RoundedCornerShape(50.dp),
+    medium = RoundedCornerShape(bottomEnd = 16.dp, topStart = 16.dp, topEnd = 16.dp)
+)
--- app/src/main/java/net/ttiimm/morsecode/ui/theme/Type.kt
+++ app/src/main/java/net/ttiimm/morsecode/ui/theme/Type.kt
@@ -2,33 +2,20 @@ package net.ttiimm.morsecode.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import net.ttiimm.morsecode.R
 
-// Set of Material typography styles to start with
-val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+val AbrilFatface = FontFamily(
+    Font(R.font.abril_fatface_regular)
+)
+
+val AppTypography = Typography(
+    displayMedium = TextStyle(
+        fontFamily = AbrilFatface,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
+        fontSize = 24.sp
     )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
 )
