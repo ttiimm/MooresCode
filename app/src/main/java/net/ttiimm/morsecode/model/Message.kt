6bcfa431ec56ac886b5ccaa3b8d87f62c64c21a6
Tim Likarish <likarish@gmail.com>
2024-07-02 17:20:17 -0400

    [Model] First go at modeling messages

--- app/src/main/java/net/ttiimm/morsecode/model/Message.kt
+++ app/src/main/java/net/ttiimm/morsecode/model/Message.kt
@@ -3,6 +3,7 @@ package net.ttiimm.morsecode.model
 import java.time.Instant
 
 data class Message(
-    val message: String,
-    val datetime: Instant
+    val content: String,
+    val datetime: Instant,
+    val isMine: Boolean
 )
