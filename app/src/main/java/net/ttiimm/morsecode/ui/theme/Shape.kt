914cd7f0d7b96fbd526ba753992512c20bbe26b4
Tim Likarish <likarish@gmail.com>
2024-07-02 23:49:22 -0400

    [UI] Style the chat bubbles to distinguish them

--- app/src/main/java/net/ttiimm/morsecode/ui/theme/Shape.kt
+++ app/src/main/java/net/ttiimm/morsecode/ui/theme/Shape.kt
@@ -6,5 +6,4 @@ import androidx.compose.ui.unit.dp
 
 val Shapes = Shapes(
     small = RoundedCornerShape(50.dp),
-    medium = RoundedCornerShape(bottomEnd = 16.dp, topStart = 16.dp, topEnd = 16.dp)
 )
