3624b0ae137df2f131e80b8c175adf5b640f8d9d
Tim Likarish <likarish@gmail.com>
2024-07-04 15:41:03 -0400

    [UI State] Move the Message into ChatUiState

--- app/src/main/java/net/ttiimm/morsecode/ui/ChatUiState.kt
+++ app/src/main/java/net/ttiimm/morsecode/ui/ChatUiState.kt
@@ -1,8 +1,22 @@
 package net.ttiimm.morsecode.ui
 
 import net.ttiimm.morsecode.data.FakeExampleMessages
+import java.time.Instant
 
 data class ChatUiState(
 //    val messages: MutableList<Message> = mutableStateListOf(),
     val messages: MutableList<Message> = FakeExampleMessages,
 )
+
+data class Message(
+    val content: String,
+    val datetime: Instant,
+    val status: MessageState
+)
+
+enum class MessageState {
+    SENDING,
+    SENT,
+    RECEIVING,
+    RECEIVED
+}
